ba9e9a32889c82790ff5a87e1678aaff53877aff
Sergi Fernandez <s.nandes@hotmail.com>
2023-02-20 10:57:57 +0000

    Parse bs4

--- web1.py
+++ web1.py
@@ -1,12 +1,35 @@
-
+import urllib3
 
 class Client(object):
     
     def __init__(self) -> None:
-        pass
+        self.url = "http://99-bottles-of-beer.net/language-python-808.html"
+
+    def get_web(self):
+        # connect url
+        httppool = urllib3.PoolManager()
+        # get html
+        resultat = httppool.request("GET", self.url)
+        # retorn html
+        return resultat.data.decode('utf-8')
+         
+
+    def parse_html(self, html):
+        # convertir bs4
+        soup = bs4.BeautifulSoup(html, features="html.parser")
+        # buscar bs4 -> codi
+        element_div = soup.find_all('div', attrs={"id":"main"})[0]
+        codi = element_div.find_all('pre')[0]
+        # retornar codi
+        return codi.text
 
     def get_data(self):
-        pass
+        # descarregar web
+        dades = self.get_web()
+        # llegir html
+        dades = self.parse_html(dades)
+        # retornar dades
+        return dades
 
 if __name__=="__main__":
     client = Client()
